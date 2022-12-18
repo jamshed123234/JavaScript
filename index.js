5b9e153a0d7993dd591531faf707fc1a2a34a740
Jamshed Gopang <>
2022-12-18 15:06:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,147 @@
+// //for in loop////
+// let obj ={
+//   ali : 54,
+//   ahemed: 88,
+//   jamshed :345
+// }
+// for (let number in obj){
+//   console.log("Marks of " + number + " are " + obj[number])
+// }
+
+// function plus(x,y){
+// return (x+y)
+// }
+// let a = 10
+// let b = 14
+// let c = 13
+// console.log(plus (a,b))
+// console.log(plus (b,c))
+// console.log(plus (c,a))
+
+
+// class form {
+//   submit (){
+//     alert(this.name  + "the form is submitted" + this.train)
+//   }
+//   cencel (){
+//     alert(this.name  + "your form is cencled" + this.train)
+//   }
+//   fill(name,train){
+//     this.name = name
+//     this.train = train
+//   }
+// }
+// let noman = new form()
+// noman.fill("noman", 45)
+// noman.submit()
+// noman.cencel()
+
+// class form{
+//   submit(){
+//    alert(this.name  + "the form is submitted" + this.train)
+//   }
+//   cencle(){
+    
+//     alert(this.name  + "the form is cencled" + this.train)
+//   }
+//   fill(name , train){
+//     this.name = name
+//     this.train = train
+//   }
+//   fill(name, train){
+//     this.name = name
+//     this.train = train
+//   }
+// }
+// let noman = new form()
+// noman.fill("noman", 67)
+// noman.submit()
+// console.log(this.name)
+// class MySelf{
+//   start (){
+//     alert(`My Name  is ${this.name} I like ${this.like}`)
+//   }
+//   fill (name, like){
+//   this.name = name
+//   this.like = like
+
+//   }
+// }
+// let jamshed= new MySelf()
+// jamshed.fill("Jamshed", "JavaSript")
+// jamshed.start()
+// class form{
+//   start(){
+//     alert("my name is "+this.name+" And My Age Is"+this.age)
+//   }
+// fill(name, age){
+//   this.name = name
+//   this.age = age
+// }
+// }
+// let jamshed =new form()
+// jamshed.fill("Jamshed",18)
+// jamshed.start()
+// class form{
+//   start(){
+//     alert(`My Name Is ${this.Name} And My Father Name is ${this.FatherName}`)
+//   }
+//   fill(Name,FatherName){
+//     this.Name = Name
+//     this.FatherName = FatherName
+//   }
+// }
+// let jamshed = new form()
+// jamshed.fill("Jamshed","Punhoon")
+// jamshed.start()
+// class form{
+//   start(){
+//     alert(`My Name Is ${this.name} And I Like ${this.like}` )
+//   }
+//   fill(name,like){
+//     this.name = name
+//     this.like = like
+//   }
+// }
+// let jamshed = new form()
+// jamshed.fill("Jamshed", "JavaSript")
+// jamshed.start()
+
+// class form {
+//   start(){
+//     alert(`My Name Is ${this.name} And MY Age IS ${this.age}`)
+//   }
+//   fill(name,age){
+//     // this.name = name
+//     // this.age = age
+//   }
+// }
+// jamshed= new form()
+// jamshed.fill("Jamshed",18)
+// jamshed.start()
+
+
+
+
+// class student{
+//   constructor(name,age){
+//     this.name = name
+//     this.age = age
+//     alert(`My Name Is ${this.name} My Age IS ${this.age}`)
+//   }
+//   // fill(name,age){
+//   //   alert(`My Name Is ${this.name} My Age IS ${this.age}`)
+//   // }
+// }
+// let a = new student("Jamshed",18)
+//     // a.fill("jamshed",37)
+
+// class student{
+//   constructor(name,age){
+//     this.name = name
+//     this.age = age
+//     alert(`My name is ${this.name} and my age is ${this.age}`)
+//   }
+// }
+// new  student("Jamshed",18)
+// new  student("noman",28)
